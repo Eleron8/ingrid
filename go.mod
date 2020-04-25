7677f814d8db5b4365f493f0383e6b707ae566a1
Almas <eleron888@gmail.com>
2020-04-25 06:02:36 +0200

    Dockerfile

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ingrid
+
+go 1.13
--- main.go
+++ main.go
@@ -11,23 +11,34 @@ import (
 )
 
 func RoutesFinder(w http.ResponseWriter, req *http.Request) {
-	url := req.URL
-	urlString := url.String()
-	paramsStr := strings.Split(urlString, "?")[1]
-	params := strings.Split(paramsStr, "&")
-	var input [][]string
-	for _, v := range params {
-		value := strings.Split(v, "=")[1]
-		dst := strings.Split(value, ",")
-		input = append(input, dst)
+	if req.Method == "GET" {
+		url := req.URL
+		urlString := url.String()
+		paramsStr := strings.Split(urlString, "?")[1]
+		params := strings.Split(paramsStr, "&")
+		var input [][]string
+		for _, v := range params {
+			value := strings.Split(v, "=")[1]
+			dst := strings.Split(value, ",")
+			input = append(input, dst)
+		}
+		data, err := data.Request(input)
+		if err != nil {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(400)
+			if err := json.NewEncoder(w).Encode(err); err != nil {
+				panic(err)
+			}
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(data)
+
+	} else {
+		http.Error(w, "Bad Request method. Need method GET", 400)
 	}
-	data, err := data.Request(input)
-	if err != nil {
-		panic(err)
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(data)
+
 }
 
 func main() {
